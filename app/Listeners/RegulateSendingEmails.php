182a6d977e1de0519ead1750f85aa9fa0843ff43
Amin Valizade <aminvalizade20@gmail.com>
2025-08-16 11:09:24 +0330

    commited it

--- app/Listeners/RegulateSendingEmails.php
+++ app/Listeners/RegulateSendingEmails.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\NoteCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class RegulateSendingEmails
 {
@@ -22,7 +23,8 @@ class RegulateSendingEmails
     public function handle(NoteCreated $event): void
     {
             if($event->note['send_date']== null) {
-
+             //  Mail::to($event->recipientEmail)->send()
+                info([$event]);
             }
     }
 }
